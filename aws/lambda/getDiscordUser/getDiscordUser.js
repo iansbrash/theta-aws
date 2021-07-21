88433395535007d9d9bf0aa67b182bdb260c44ae
Ian Brash <brash@usc.edu>
2021-07-21 13:40:07 -0500

    Add getDiscordUser

---
+++ aws/lambda/getDiscordUser/getDiscordUser.js
@@ -0,0 +1,39 @@
+var AWS = require("aws-sdk");
+AWS.config.update({
+    region: "us-east-1",
+});
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async function(event, context) {        
+    console.log('Received event:', JSON.stringify(event, null, 2));
+
+    // Retrieve request parameters from the Lambda function input:
+    var headers = event.headers;
+        
+    const params = {
+        TableName: 'licenseKeys',
+        Key:
+        {
+            "license": headers.license
+        }
+    }
+    
+    let result = await docClient.get(params).promise();
+    
+    if (result.Item !== undefined && result.Item !== null) {
+        
+        return {
+            "isBase64Encoded": false,
+            "statusCode": 200,
+            "body": JSON.stringify(result.Item.discordId)
+        }
+        
+    }
+    else {
+        return {
+            "isBase64Encoded": false,
+            "statusCode": 404,
+            "body": "License key invalid"
+        }
+    }
+}
